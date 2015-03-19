501f1bf7dee2c8fa9e416769a9533b137b31260b
Dan Koepke <dan.koepke@appdynamics.com>
2015-03-19 11:45:41 -0700

    Update verify script to run Foodcritic and Rubocop.

--- verify.sh
+++ verify.sh
@@ -1,3 +1,30 @@
-bin/rspec
-bundle exec kitchen converge
+if [[ $# == 0 ]]; then
+    KITCHEN_SUBCOMMAND="verify"
+else
+    KITCHEN_SUBCOMMAND="$*"
+fi
 
+report() {
+    local message=$1
+    shift
+
+    echo -n "Running $message... "
+
+    local results=$(bundle exec $* 2>&1)
+
+    if [[ $? == 0 ]]; then
+        echo OK!
+    else
+        echo Failed
+        echo
+        echo $results
+        echo
+    fi
+}
+
+report "RSpec" rspec spec
+report "Foodcritic" foodcritic --epic-fail any .
+report "Rubocop" rubocop
+
+echo "Running Kitchen ($KITCHEN_SUBCOMMAND)..."
+bundle exec kitchen $KITCHEN_SUBCOMMAND
