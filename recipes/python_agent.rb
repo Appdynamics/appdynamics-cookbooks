78e06afe41879693173e11f8929fd436cf082a85
Dan Koepke <dan.koepke@appdynamics.com>
2015-03-18 21:00:39 -0700

    Clean up Python agent recipe.

--- recipes/python_agent.rb
+++ recipes/python_agent.rb
@@ -1,34 +1,38 @@
+agent = node['appdynamics']['python_agent']
+controller = node['appdynamics']['controller']
+http_proxy = node['appdynamics']['http_proxy']
+
 python_pip 'appdynamics' do
-  virtualenv node['appdynamics']['python_agent']['virtualenv'] if node['appdynamics']['python_agent']['virtualenv']
-  action node['appdynamics']['python_agent']['action']
-  version node['appdynamics']['python_agent']['version'] if node['appdynamics']['python_agent']['version'] != 'latest'
-  options '--pre' if node['appdynamics']['python_agent']['prerelease']
+  virtualenv agent['virtualenv'] if agent['virtualenv']
+  action agent['action']
+  version agent['version'] if agent['version'] != 'latest'
+  options '--pre' if agent['prerelease']
 end
 
-template node['appdynamics']['python_agent']['config_file'] do
-  cookbook node['appdynamics']['python_agent']['template']['cookbook']
-  source node['appdynamics']['python_agent']['template']['source']
-  owner node['appdynamics']['python_agent']['template']['user']
-  owner node['appdynamics']['python_agent']['template']['group']
-  mode node['appdynamics']['python_agent']['template']['mode']
+template agent['config_file'] do
+  cookbook agent['template']['cookbook']
+  source agent['template']['source']
+  owner agent['template']['user']
+  owner agent['template']['group']
+  mode agent['template']['mode']
 
   variables(
     :app_name => node['appdynamics']['app_name'],
     :tier_name => node['appdynamics']['tier_name'],
     :node_name => node['appdynamics']['node_name'],
 
-    :controller_host => node['appdynamics']['controller']['host'],
-    :controller_port => node['appdynamics']['controller']['port'],
-    :controller_ssl => node['appdynamics']['controller']['ssl'],
-    :controller_user => node['appdynamics']['controller']['user'],
-    :controller_accesskey => node['appdynamics']['controller']['accesskey'],
+    :controller_host => controller['host'],
+    :controller_port => controller['port'],
+    :controller_ssl => controller['ssl'],
+    :controller_user => controller['user'],
+    :controller_accesskey => controller['accesskey'],
 
-    :http_proxy_host => node['appdynamics']['controller']['http_proxy']['host'],
-    :http_proxy_port => node['appdynamics']['controller']['http_proxy']['port'],
-    :http_proxy_user => node['appdynamics']['controller']['http_proxy']['user'],
-    :http_proxy_password_file => node['appdynamics']['controller']['http_proxy']['password_file'],
+    :http_proxy_host => controller['http_proxy']['host'],
+    :http_proxy_port => controller['http_proxy']['port'],
+    :http_proxy_user => controller['http_proxy']['user'],
+    :http_proxy_password_file => controller['http_proxy']['password_file'],
 
-    :debug => node['appdynamics']['python_agent']['debug'],
-    :dir => node['appdynamics']['python_agent']['dir']
+    :debug => agent['debug'],
+    :dir => agent['dir']
   )
 end
