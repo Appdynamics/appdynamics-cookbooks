8ad8b9ebc394fe6c465e5e8ae5ef79fb9b7eaa11
Al Kemner <al.kemner@gmail.com>
2015-10-01 18:33:49 -0400

    dotnet_agent - add support for dynamic integration test on windows

--- test/fixtures/cookbooks/test-helper/recipes/default.rb
+++ test/fixtures/cookbooks/test-helper/recipes/default.rb
@@ -3,16 +3,28 @@ chef_gem 'activesupport'
 require 'pathname'
 require 'active_support/core_ext/hash/deep_merge'
 
-directory '/tmp/serverspec' do
-  recursive true
+case node['platform_family']
+when 'debian', 'rhel', 'fedora'
+  temp_path = '/tmp/serverspec/'
+  directory temp_path do
+    recursive true
+  end
+
+  file "#{temp_path}node.json" do
+    owner 'root'
+    mode '0400'
+  end
+when 'windows'
+  temp_path = 'c:\\windows\\temp\\serverspec\\'
+  directory temp_path do
+    recursive true
+  end
+
+  file "#{temp_path}node.json" do
+  end
 end
 
-file '/tmp/serverspec/node.json' do
-  owner 'root'
-  mode '400'
-end
-
-log "Dumping attributes to '/tmp/serverspec/node.json."
+log "Dumping attributes to 'node.json'"
 
 ruby_block 'dump_node_attributes' do
   block do
@@ -20,6 +32,7 @@ ruby_block 'dump_node_attributes' do
 
     attrs = {}
 
+    attrs = attrs.deep_merge(node.automatic_attrs) unless node.automatic_attrs.empty?
     attrs = attrs.deep_merge(node.default_attrs) unless node.default_attrs.empty?
     attrs = attrs.deep_merge(node.normal_attrs) unless node.normal_attrs.empty?
     attrs = attrs.deep_merge(node.override_attrs) unless node.override_attrs.empty?
@@ -29,6 +42,6 @@ ruby_block 'dump_node_attributes' do
     recipe_json << " \] }"
     attrs = attrs.deep_merge(JSON.parse(recipe_json))
 
-    File.open('/tmp/serverspec/node.json', 'w') { |file| file.write(JSON.pretty_generate(attrs)) }
+    File.open("#{temp_path}node.json", 'w') { |file| file.write(JSON.pretty_generate(attrs)) }
   end
 end
--- test/integration/dotnet_agent/serverspec/spec_helper.rb
+++ test/integration/dotnet_agent/serverspec/spec_helper.rb
@@ -1,4 +1,8 @@
 require 'serverspec'
+require 'pathname'
+require 'json'
 
 set :backend, :cmd
 set :os, :family => 'windows'
+
+$node = ::JSON.parse(File.read('c:\windows\temp\serverspec\node.json'))
